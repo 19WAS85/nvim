d884b3367ce2e00794b8fe5e350a08a0d9219834
Wagner Andrade <19WAS85@gmail.com>
2024-01-03 23:38:02 -0300

    feat: basic custom options

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,18 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+local map = vim.keymap.set
+
+-- BUFFERS
+
+map("n", "<tab>", "<cmd>BufferLineCycleNext<cr>", { desc = "Next buffer" })
+map("n", "<S-tab>", "<cmd>BufferLineCyclePrev<cr>", { desc = "Prev buffer" })
+
+map("n", "<S-q>", function()
+  require("mini.bufremove").delete(0, true)
+end, { desc = "Delete Buffer" })
+
+-- NEOTREE
+
+map("n", "\\", ":Neotree<cr>", { desc = "Neotree Focus" })
