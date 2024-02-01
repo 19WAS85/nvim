9241e9f510552e854c9398230f98b7b3b0ef3885
Wagner Andrade <19WAS85@gmail.com>
2024-02-01 11:50:36 -0300

    add dap and xcode theme

--- lua/plugins/index.lua
+++ lua/plugins/index.lua
@@ -1,4 +1,6 @@
 return {
   { "echasnovski/mini.pairs", enabled = false },
   { "folke/flash.nvim", opts = { modes = { search = { enabled = false } } } },
+  { "lunacookies/vim-colors-xcode" },
+  { "LazyVim/LazyVim", opts = { colorscheme = "xcodelight" } },
 }
