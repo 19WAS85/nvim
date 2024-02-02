ceebd9001ec5b44d633ebb7258e9f04a777b1db3
Wagner Andrade <19WAS85@gmail.com>
2024-02-02 11:12:31 -0300

    install theme

--- lua/plugins/index.lua
+++ lua/plugins/index.lua
@@ -2,5 +2,6 @@ return {
   { "echasnovski/mini.pairs", enabled = false },
   { "folke/flash.nvim", opts = { modes = { search = { enabled = false } } } },
   { "lunacookies/vim-colors-xcode" },
-  { "LazyVim/LazyVim", opts = { colorscheme = "xcodelight" } },
+  { "rose-pine/neovim" },
+  { "LazyVim/LazyVim", opts = { colorscheme = "rose-pine" } },
 }
