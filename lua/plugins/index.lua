3696f1c509d212a7d9a882e0d7ebd1bdeac03c0b
Wagner Andrade <19WAS85@gmail.com>
2024-06-03 12:07:07 -0300

    chore: update

--- lua/plugins/index.lua
+++ lua/plugins/index.lua
@@ -1,24 +1,18 @@
 return {
   -- Disable
+  { "garymjr/nvim-snippets", enabled = false },
   { "echasnovski/mini.pairs", enabled = false },
   { "folke/flash.nvim", opts = { modes = { search = { enabled = false } } } },
 
   -- Colors
   { "rose-pine/neovim" },
   { "lunacookies/vim-colors-xcode" },
-  { "miikanissi/modus-themes.nvim" },
-  { "dracula/vim" },
 
   -- LazyVim
   {
     "LazyVim/LazyVim",
     opts = {
-      -- colorscheme = "rose-pine",
-      -- colorscheme = "rose-pine-dawn",
       colorscheme = "rose-pine-moon",
-      -- colorscheme = "xcodelight",
-      -- colorscheme = "modus",
-      -- colorscheme = "dracula",
     },
   },
 }
