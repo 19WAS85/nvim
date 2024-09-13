c8a72249aae2c0d6236c32161a1fe6a4623c34a1
Wagner Andrade <19WAS85@gmail.com>
2024-09-13 15:49:06 -0300

    chore: remove lualine_c section

--- lua/plugins/index.lua
+++ lua/plugins/index.lua
@@ -11,5 +11,12 @@ return {
 
   -- EDIT ---------------------------------------------------------------------
   { "hrsh7th/nvim-cmp", opts = { completion = { autocomplete = false } } },
-  { "nvim-lspconfig", opts = { inlay_hints = { enabled = false } } }
+  { "nvim-lspconfig", opts = { inlay_hints = { enabled = false } } },
+  {
+    "nvim-lualine/lualine.nvim",
+    event = "VeryLazy",
+    opts = function(_, opts)
+      table.remove(opts.sections.lualine_c)
+    end,
+  }
 }
