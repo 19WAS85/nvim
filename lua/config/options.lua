d156c2ee60180f400ad4501241aed645e3f46b74
Wagner Andrade <19WAS85@gmail.com>
2024-09-26 12:07:35 -0300

    refactor: organize code

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -3,15 +3,13 @@
 -- Add any additional options here
 
 vim.g.autoformat = false
-
-vim.opt.guicursor = vim.opt.guicursor + 'a:blinkon100'
+vim.g.root_spec = { 'cwd' }
 
 vim.opt.colorcolumn = '80'
 vim.opt.cursorline = false
+vim.opt.guicursor = vim.opt.guicursor + 'a:blinkon100'
 vim.opt.list = false
 vim.opt.relativenumber = false
 vim.opt.spelllang = {}
 vim.opt.swapfile = false
 vim.opt.undofile = false
-
-vim.g.root_spec = { 'cwd' }
--- lua/plugins/index.lua
+++ lua/plugins/index.lua
@@ -11,23 +11,35 @@ return {
 
   -- EDIT ---------------------------------------------------------------------
 
-  { "hrsh7th/nvim-cmp", opts = { completion = { autocomplete = false } } },
+  {
+    "nvimdev/dashboard-nvim",
+    lazy = false,
+    opts = function(_, opts)
+      opts.config.header = { "", "", "https://www.lazyvim.org/", "", "" }
+    end,
+  },
 
-  { "nvim-lspconfig", opts = { inlay_hints = { enabled = false } } },
+  {
+    "nvim-lspconfig",
+    event = "VeryLazy",
+    opts = function(_, opts)
+      opts.inlay_hints.enabled = false
+    end,
+  },
+
+  {
+    "hrsh7th/nvim-cmp",
+    event = "VeryLazy",
+    opts = function(_, opts)
+      opts.completion.autocomplete = false
+    end,
+  },
 
   {
     "nvim-lualine/lualine.nvim",
     event = "VeryLazy",
     opts = function(_, opts)
       table.remove(opts.sections.lualine_c)
-    end,
-  },
-
-  {
-    "nvimdev/dashboard-nvim",
-    lazy = false,
-    opts = function(_, opts)
-      opts.config.header = { "", "", "https://www.lazyvim.org/", "", "" }
     end,
   }
 }
