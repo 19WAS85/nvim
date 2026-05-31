5f85449a8829b9de484436d710ca971fcbdcaf36
Wagner Andrade <wagner.andrade@sap.com>
2026-05-30 21:13:53 -0300

    feat(like): add syntax and treesitter highlight groups

--- colors/like.lua
+++ colors/like.lua
@@ -92,4 +92,84 @@ h.Question        = { fg = p.func }
 h.WarningMsg      = { fg = p.warning }
 h.ErrorMsg        = { fg = p.error }
 
+-- Legacy syntax
+h.Comment         = { fg = p.comment, italic = true }
+h.Keyword         = { fg = p.keyword, bold = true }
+h.KeywordFunction = { fg = p.keyword, bold = true }
+h.KeywordReturn   = { fg = p.keyword, bold = true }
+h.KeywordOperator = { fg = p.keyword }
+h.String          = { fg = p.string }
+h.Character       = { fg = p.string }
+h.Number          = { fg = p.number }
+h.Float           = { fg = p.number }
+h.Boolean         = { fg = p.keyword, bold = true }
+h.Operator        = { fg = p.keyword }
+h.Delimiter       = { fg = p.fg_dark }
+h.Function        = { fg = p.func }
+h.Method          = { fg = p.func }
+h.Type            = { fg = p.type }
+h.TypeDef         = { fg = p.type }
+h.Identifier      = { fg = p.variable }
+h.Constant        = { fg = p.constant }
+h.Special         = { fg = p.symbol }
+h.SpecialChar     = { fg = p.string }
+h.SpecialComment  = { fg = p.comment, bold = true }
+h.Tag             = { fg = p.tag }
+h.Attribute       = { fg = p.variable }
+h.PreProc         = { fg = p.keyword }
+h.Include         = { fg = p.keyword }
+h.Define          = { fg = p.keyword }
+h.Macro           = { fg = p.keyword }
+h.Statement       = { fg = p.keyword, bold = true }
+h.Conditional     = { fg = p.keyword, bold = true }
+h.Repeat          = { fg = p.keyword, bold = true }
+h.Label           = { fg = p.func }
+h.Exception       = { fg = p.func }
+h.Structure       = { fg = p.type }
+h.StorageClass    = { fg = p.type }
+h.Typedef         = { fg = p.type }
+h.Todo            = { fg = p.builtin, bold = true }
+h.Error           = { fg = p.error,   bg = p.error_bg }
+h.Underlined      = { fg = p.tag,     underline = true }
+
+-- Treesitter
+h["@comment"]               = { link = "Comment" }
+h["@keyword"]               = { link = "Keyword" }
+h["@keyword.function"]      = { link = "KeywordFunction" }
+h["@keyword.return"]        = { link = "KeywordReturn" }
+h["@keyword.operator"]      = { link = "KeywordOperator" }
+h["@string"]                = { link = "String" }
+h["@string.regex"]          = { fg = p.regex }
+h["@string.escape"]         = { fg = p.symbol }
+h["@number"]                = { link = "Number" }
+h["@float"]                 = { link = "Float" }
+h["@boolean"]               = { link = "Boolean" }
+h["@operator"]              = { link = "Operator" }
+h["@punctuation.delimiter"] = { fg = p.fg_dark }
+h["@punctuation.bracket"]   = { fg = p.fg_dark }
+h["@function"]              = { link = "Function" }
+h["@function.builtin"]      = { fg = p.builtin }
+h["@method"]                = { link = "Method" }
+h["@type"]                  = { link = "Type" }
+h["@type.builtin"]          = { fg = p.type, italic = true }
+h["@variable"]              = { fg = p.variable }
+h["@variable.builtin"]      = { fg = p.builtin, italic = true }
+h["@constant"]              = { link = "Constant" }
+h["@constant.builtin"]      = { fg = p.constant, bold = true }
+h["@namespace"]             = { fg = p.namespace }
+h["@attribute"]             = { fg = p.variable }
+h["@tag"]                   = { fg = p.tag }
+h["@tag.attribute"]         = { fg = p.variable }
+h["@tag.delimiter"]         = { fg = p.fg_dark }
+h["@constructor"]           = { fg = p.type }
+h["@parameter"]             = { fg = p.fg }
+h["@field"]                 = { fg = p.variable }
+h["@property"]              = { fg = p.variable }
+h["@symbol"]                = { fg = p.symbol }
+h["@text.literal"]          = { fg = p.string }
+h["@text.uri"]              = { fg = p.builtin, underline = true }
+h["@text.title"]            = { fg = p.type,    bold = true }
+h["@text.emphasis"]         = { italic = true }
+h["@text.strong"]           = { bold = true }
+
 -- Section 3: Application (populated in Task 6)
