9e10c557a71306138b08a865b8e56d4f0859b41d
Wagner Andrade <wagner.andrade@sap.com>
2026-05-30 21:55:50 -0300

    feat(keymaps): add buffer and explorer navigation shortcuts
    - Tab/S-Tab: cycle buffers (bnext/bprevious)
    - S-q: close buffer via Snacks.bufdelete (preserves window layout)
    - |: open explorer or focus it if already open

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,15 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+vim.keymap.set("n", "<Tab>", "<cmd>bnext<cr>", { desc = "Next buffer" })
+vim.keymap.set("n", "<S-Tab>", "<cmd>bprevious<cr>", { desc = "Prev buffer" })
+vim.keymap.set("n", "<S-q>", function() Snacks.bufdelete.delete() end, { desc = "Close buffer" })
+vim.keymap.set("n", "|", function()
+  local explorer = Snacks.picker.get({ source = "explorer" })[1]
+  if explorer then
+    explorer:focus()
+  else
+    Snacks.explorer()
+  end
+end, { desc = "Open explorer" })
